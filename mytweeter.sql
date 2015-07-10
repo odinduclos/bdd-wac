cb72dd29b5cfa588d8605b089c6b4cda03fb17a2
ENAUX Guillaume <guillaume.enaux@epitech.eu>
2015-07-10 17:00:38 +0200

    Change DDB

--- mytweeter.sql
+++ mytweeter.sql
@@ -66,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `members` (
   `date_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `location` varchar(50) NOT NULL,
   `profil_image` varchar(255) NOT NULL,
+  `mail` varchar(75) CHARACTER SET utf8 NOT NULL
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
